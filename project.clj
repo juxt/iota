7acf2c698071a147383b95652cec3d23839862e2
Malcolm Sparks <malcolm@juxt.pro>
2015-10-16 13:02:16 +0100

    Release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2015, JUXT LTD.
 
-(defproject juxt/iota "0.2.0"
+(defproject juxt/iota "0.2.1"
   :description "Infix operators for test assertions"
   :url "http://github.com/juxt/iota"
   :license {:name "The MIT License"
